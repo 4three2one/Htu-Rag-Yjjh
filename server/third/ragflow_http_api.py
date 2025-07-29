e74ce83c77df92637f669edc46f7b7090e3348f8
xujiawei <1375464896@qq.com>
2025-07-29 13:44:57 +0800

    更新代码

--- server/third/ragflow_http_api.py
+++ server/third/ragflow_http_api.py
@@ -7,13 +7,14 @@ import json
 from typing import AsyncGenerator
 
 # 配置 ragflow HTTP API 基础地址和 API KEY
-api_key = os.getenv("RAGFLOW_API_KEY", "r")
+api_key = os.getenv("RAGFLOW_API_KEY", "")
 base_url = os.getenv("RAGFLOW_BASE_URL", "")
+chat_id=os.getenv("RAGFLOW_CHAT_ID", "")
+
 
 headers = {
     "Authorization": f"Bearer {api_key}"
 }
-chat_id: str = "be0d226a63a211f0a894822a712eb46f"
 
 
 async def list_documents_http(
