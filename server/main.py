702675e8e2327fb42e0711a379311b335c3159da
xujiawei <1375464896@qq.com>
2025-08-01 17:42:14 +0800

    提交修改

--- server/main.py
+++ server/main.py
@@ -59,5 +59,5 @@ app.add_middleware(AuthMiddleware)
 
 if __name__ == "__main__":
     # uvicorn.run(app, host="0.0.0.0", port=5050, workers=10, reload=True)
-    uvicorn.run("main:app", host="0.0.0.0", port=5050, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=5050, reload=True)
 
